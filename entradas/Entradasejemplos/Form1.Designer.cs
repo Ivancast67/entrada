0bd7fbcc647cdd03499804e42bc712b489ee557b
Ivancast67 <166355701+Ivancast67@users.noreply.github.com>
2024-10-30 08:44:17 -0600

    Add files via upload

---
+++ entradas/Entradasejemplos/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Entradasejemplos
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Variable del diseñador necesaria.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Limpiar los recursos que se estén usando.
+      /// </summary>
+      /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Código generado por el Diseñador de Windows Forms
+
+      /// <summary>
+      /// Método necesario para admitir el Diseñador. No se puede modificar
+      /// el contenido de este método con el editor de código.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.components = new System.ComponentModel.Container();
+         this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+         this.label1 = new System.Windows.Forms.Label();
+         this.textBox1 = new System.Windows.Forms.TextBox();
+         this.label2 = new System.Windows.Forms.Label();
+         this.label3 = new System.Windows.Forms.Label();
+         this.textBox2 = new System.Windows.Forms.TextBox();
+         this.metroTile1 = new MetroFramework.Controls.MetroTile();
+         this.label4 = new System.Windows.Forms.Label();
+         this.textBox3 = new System.Windows.Forms.TextBox();
+         this.metroTile2 = new MetroFramework.Controls.MetroTile();
+         this.metroTile3 = new MetroFramework.Controls.MetroTile();
+         this.metroTile4 = new MetroFramework.Controls.MetroTile();
+         this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+         this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager(this.components);
+         this.metroToggle1 = new MetroFramework.Controls.MetroToggle();
+         ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).BeginInit();
+         this.SuspendLayout();
+         // 
+         // metroTextBox1
+         // 
+         // 
+         // 
+         // 
+         this.metroTextBox1.CustomButton.Image = null;
+         this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(53, 1);
+         this.metroTextBox1.CustomButton.Name = "";
+         this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
+         this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+         this.metroTextBox1.CustomButton.TabIndex = 1;
+         this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+         this.metroTextBox1.CustomButton.UseSelectable = true;
+         this.metroTextBox1.CustomButton.Visible = false;
+         this.metroTextBox1.Lines = new string[0];
+         this.metroTextBox1.Location = new System.Drawing.Point(368, 164);
+         this.metroTextBox1.MaxLength = 250;
+         this.metroTextBox1.Multiline = true;
+         this.metroTextBox1.Name = "metroTextBox1";
+         this.metroTextBox1.PasswordChar = '\0';
+         this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+         this.metroTextBox1.SelectedText = "";
+         this.metroTextBox1.SelectionLength = 0;
+         this.metroTextBox1.SelectionStart = 0;
+         this.metroTextBox1.ShortcutsEnabled = true;
+         this.metroTextBox1.Size = new System.Drawing.Size(219, 43);
+         this.metroTextBox1.TabIndex = 5;
+         this.metroTextBox1.UseSelectable = true;
+         this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+         this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+         // 
+         // label1
+         // 
+         this.label1.AutoSize = true;
+         this.label1.Location = new System.Drawing.Point(200, 87);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(79, 24);
+         this.label1.TabIndex = 0;
+         this.label1.Text = "Nombre";
+         // 
+         // textBox1
+         // 
+         this.textBox1.Location = new System.Drawing.Point(378, 87);
+         this.textBox1.MaxLength = 60;
+         this.textBox1.Name = "textBox1";
+         this.textBox1.Size = new System.Drawing.Size(209, 29);
+         this.textBox1.TabIndex = 4;
+         // 
+         // label2
+         // 
+         this.label2.AutoSize = true;
+         this.label2.Location = new System.Drawing.Point(188, 164);
+         this.label2.Name = "label2";
+         this.label2.Size = new System.Drawing.Size(110, 24);
+         this.label2.TabIndex = 1;
+         this.label2.Text = "Descripcion";
+         // 
+         // label3
+         // 
+         this.label3.AutoSize = true;
+         this.label3.Location = new System.Drawing.Point(188, 246);
+         this.label3.Name = "label3";
+         this.label3.Size = new System.Drawing.Size(64, 24);
+         this.label3.TabIndex = 2;
+         this.label3.Text = "Precio";
+         // 
+         // textBox2
+         // 
+         this.textBox2.Location = new System.Drawing.Point(375, 243);
+         this.textBox2.Name = "textBox2";
+         this.textBox2.Size = new System.Drawing.Size(212, 29);
+         this.textBox2.TabIndex = 6;
+         // 
+         // metroTile1
+         // 
+         this.metroTile1.ActiveControl = null;
+         this.metroTile1.BackColor = System.Drawing.Color.DarkGreen;
+         this.metroTile1.Location = new System.Drawing.Point(159, 397);
+         this.metroTile1.Name = "metroTile1";
+         this.metroTile1.Size = new System.Drawing.Size(93, 42);
+         this.metroTile1.TabIndex = 6;
+         this.metroTile1.Text = "Guardar";
+         this.metroTile1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+         this.metroTile1.UseCustomBackColor = true;
+         this.metroTile1.UseSelectable = true;
+         this.metroTile1.UseTileImage = true;
+         // 
+         // label4
+         // 
+         this.label4.AutoSize = true;
+         this.label4.Location = new System.Drawing.Point(188, 315);
+         this.label4.Name = "label4";
+         this.label4.Size = new System.Drawing.Size(56, 24);
+         this.label4.TabIndex = 3;
+         this.label4.Text = "Stock";
+         // 
+         // textBox3
+         // 
+         this.textBox3.Location = new System.Drawing.Point(381, 310);
+         this.textBox3.Name = "textBox3";
+         this.textBox3.Size = new System.Drawing.Size(212, 29);
+         this.textBox3.TabIndex = 7;
+         // 
+         // metroTile2
+         // 
+         this.metroTile2.ActiveControl = null;
+         this.metroTile2.Location = new System.Drawing.Point(685, 397);
+         this.metroTile2.Name = "metroTile2";
+         this.metroTile2.Size = new System.Drawing.Size(91, 42);
+         this.metroTile2.TabIndex = 9;
+         this.metroTile2.Text = "Buscar";
+         this.metroTile2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+         this.metroTile2.UseSelectable = true;
+         // 
+         // metroTile3
+         // 
+         this.metroTile3.ActiveControl = null;
+         this.metroTile3.Location = new System.Drawing.Point(381, 397);
+         this.metroTile3.Name = "metroTile3";
+         this.metroTile3.Size = new System.Drawing.Size(82, 42);
+         this.metroTile3.TabIndex = 10;
+         this.metroTile3.Text = "Editar";
+         this.metroTile3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+         this.metroTile3.UseSelectable = true;
+         // 
+         // metroTile4
+         // 
+         this.metroTile4.ActiveControl = null;
+         this.metroTile4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+         this.metroTile4.Location = new System.Drawing.Point(558, 397);
+         this.metroTile4.Name = "metroTile4";
+         this.metroTile4.Size = new System.Drawing.Size(85, 42);
+         this.metroTile4.TabIndex = 11;
+         this.metroTile4.Text = "Eliminar";
+         this.metroTile4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+         this.metroTile4.UseCustomBackColor = true;
+         this.metroTile4.UseSelectable = true;
+         // 
+         // metroStyleManager1
+         // 
+         this.metroStyleManager1.Owner = this;
+         // 
+         // metroToggle1
+         // 
+         this.metroToggle1.AutoSize = true;
+         this.metroToggle1.Location = new System.Drawing.Point(823, 42);
+         this.metroToggle1.Name = "metroToggle1";
+         this.metroToggle1.Size = new System.Drawing.Size(80, 28);
+         this.metroToggle1.TabIndex = 12;
+         this.metroToggle1.Text = "Off";
+         this.metroToggle1.UseSelectable = true;
+         this.metroToggle1.CheckedChanged += new System.EventHandler(this.metroToggle1_CheckedChanged);
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(1386, 788);
+         this.Controls.Add(this.metroToggle1);
+         this.Controls.Add(this.metroTile4);
+         this.Controls.Add(this.metroTile3);
+         this.Controls.Add(this.metroTile2);
+         this.Controls.Add(this.textBox3);
+         this.Controls.Add(this.label4);
+         this.Controls.Add(this.metroTile1);
+         this.Controls.Add(this.textBox2);
+         this.Controls.Add(this.label3);
+         this.Controls.Add(this.label2);
+         this.Controls.Add(this.textBox1);
+         this.Controls.Add(this.label1);
+         this.Controls.Add(this.metroTextBox1);
+         this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+         this.MaximizeBox = false;
+         this.Name = "Form1";
+         this.Padding = new System.Windows.Forms.Padding(37, 111, 37, 37);
+         this.Resizable = false;
+         this.Text = "Producto";
+         ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).EndInit();
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private MetroFramework.Controls.MetroTextBox metroTextBox1;
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.TextBox textBox1;
+      private System.Windows.Forms.Label label2;
+      private System.Windows.Forms.Label label3;
+      private System.Windows.Forms.TextBox textBox2;
+      private MetroFramework.Controls.MetroTile metroTile1;
+      private System.Windows.Forms.Label label4;
+      private System.Windows.Forms.TextBox textBox3;
+      private MetroFramework.Controls.MetroTile metroTile2;
+      private MetroFramework.Controls.MetroTile metroTile3;
+      private MetroFramework.Controls.MetroTile metroTile4;
+      private System.Windows.Forms.ToolTip toolTip1;
+      private MetroFramework.Components.MetroStyleManager metroStyleManager1;
+      private MetroFramework.Controls.MetroToggle metroToggle1;
+   }
+}
+
---
+++ entradas/Entradasejemplos/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Entradasejemplos
+{
+   public partial class Form1 : 
+      MetroFramework .Forms.MetroForm
+   {
+      public Form1()
+      {
+         InitializeComponent();
+         this.StyleManager = metroStyleManager1;
+      }
+
+      private void metroToggle1_CheckedChanged(object sender, EventArgs e)
+      {
+         if (metroToggle1.Checked) 
+         {
+            metroStyleManager1.Theme =
+               MetroFramework.MetroThemeStyle.Dark;
+         }
+         else
+         {
+            metroStyleManager1.Theme =
+               MetroFramework.MetroThemeStyle.Light;
+         }
+      }
+   }
+}
